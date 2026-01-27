24ec0926d90f243f8b6d953384b8b3f3e77ab5a7
wiliameq <wiliameq@gmail.com>
2026-01-27 22:23:34 +0100

    Revert "Dodaj zwijaną sekcję „Pomiary” w prawym docku i napraw użycie metadanych projektu"

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -579,9 +579,6 @@ void MainWindow::refreshProjectPanel(int preferredBuildingIndex, int preferredFl
 QString MainWindow::createProjectTempFile(const QString& projectName,
                                          const QString& address,
                                          const QString& investor) {
-    m_projectName = projectName;
-    m_projectAddress = address;
-    m_projectInvestor = investor;
     QString safeName = projectName;
     safeName.replace(QRegularExpression(QStringLiteral("[^\\w\\d\\- ]")), "_");
     safeName = safeName.trimmed();
